ef0a25694e5a7636376862885cee4d2c1ace366e
Stefano Ravera <68197848+Lightino@users.noreply.github.com>
2025-07-05 18:41:10 +0200

    Update authStore.js

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,34 +1,38 @@
-import { defineStore } from 'pinia'
-import { jwtDecode } from 'jwt-decode'  
+import { defineStore } from "pinia";
+import { jwtDecode } from "jwt-decode";
 
-export const useAuthStore = defineStore('auth', {
+export const useAuthStore = defineStore("auth", {
   state: () => ({
     token: null,
   }),
   getters: {
     isAuthenticated(state) {
-      if (!state.token) return false
+      if (!state.token) return false;
       try {
-        const decoded = jwtDecode(state.token)
-        const now = Date.now() / 1000
-        return decoded.exp && decoded.exp > now
+        const decoded = jwtDecode(state.token);
+        const now = Date.now() / 1000;
+        return decoded.exp && decoded.exp > now;
       } catch (e) {
-        return false
+        return false;
       }
     },
   },
   actions: {
     setToken(token) {
-      this.token = token
-      localStorage.setItem('token', token)
+      console.log("Setting token in store:", token);
+      this.token = token;
+      localStorage.setItem("token", token);
     },
     logout() {
-      this.token = null
-      localStorage.removeItem('token')
+      this.token = null;
+      localStorage.removeItem("token");
     },
     loadTokenFromStorage() {
-      const storedToken = localStorage.getItem('token')
-      this.token = storedToken
+      return new Promise((resolve) => {
+        const storedToken = localStorage.getItem("token");
+        this.token = storedToken;
+        resolve();
+      });
     },
   },
-})
+});
